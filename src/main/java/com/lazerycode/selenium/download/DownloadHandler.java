3b23f949be5b956113327141372e7b1d1f113a6f
Mark Collin <mark.collin@lazeryattack.com>
2014-10-31 07:30:46 +0000

    Extract file download out into a separate class

--- src/main/java/com/lazerycode/selenium/download/DownloadHandler.java
+++ src/main/java/com/lazerycode/selenium/download/DownloadHandler.java
@@ -4,39 +4,25 @@ import com.lazerycode.selenium.extract.BinaryFileNames;
 import com.lazerycode.selenium.extract.ExtractFilesFromArchive;
 import com.lazerycode.selenium.repository.FileDetails;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.config.SocketConfig;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Map;
-
-import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
 
 public class DownloadHandler {
 
     private static final Logger LOG = Logger.getLogger(DownloadHandler.class);
     private final File rootStandaloneServerDirectory;
     private final File downloadedZipFileDirectory;
-    private final int fileDownloadReadTimeout;
-    private final int fileDownloadConnectTimeout;
     private final Map<String, FileDetails> filesToDownload;
     final int fileDownloadRetryAttempts;
     private boolean overwriteFilesThatExist = false;
     private boolean checkFileHash = true;
-    private boolean useSystemProxy = true;
+    private FileDownloader fileDownloader;
 
-    public DownloadHandler(File rootStandaloneServerDirectory, File downloadedZipFileDirectory, int fileDownloadRetryAttempts, int fileDownloadConnectTimeout, int fileDownloadReadTimeout, Map<String, FileDetails> filesToDownload, boolean overwriteFilesThatExist, boolean checkFileHash, boolean useSystemProxy) {
+    public DownloadHandler(File rootStandaloneServerDirectory, File downloadedZipFileDirectory, int fileDownloadRetryAttempts, int fileDownloadConnectTimeout, int fileDownloadReadTimeout, Map<String, FileDetails> filesToDownload, boolean overwriteFilesThatExist, boolean checkFileHash, boolean useSystemProxy) throws MojoFailureException {
         this.rootStandaloneServerDirectory = rootStandaloneServerDirectory;
         this.downloadedZipFileDirectory = downloadedZipFileDirectory;
         if (fileDownloadRetryAttempts < 1) {
@@ -45,12 +31,13 @@ public class DownloadHandler {
         } else {
             this.fileDownloadRetryAttempts = fileDownloadRetryAttempts;
         }
-        this.fileDownloadConnectTimeout = fileDownloadConnectTimeout;
-        this.fileDownloadReadTimeout = fileDownloadReadTimeout;
         this.filesToDownload = filesToDownload;
         this.overwriteFilesThatExist = overwriteFilesThatExist;
         this.checkFileHash = checkFileHash;
-        this.useSystemProxy = useSystemProxy;
+
+        this.fileDownloader = new FileDownloader(downloadedZipFileDirectory, useSystemProxy);
+        this.fileDownloader.setReadTimeout(fileDownloadReadTimeout);
+        this.fileDownloader.setConnectTimeout(fileDownloadConnectTimeout);
     }
 
     public void getStandaloneExecutableFiles() throws Exception {
@@ -93,72 +80,31 @@ public class DownloadHandler {
      * @throws MojoExecutionException
      */
     File downloadFile(FileDetails fileDetails) throws Exception {
-        String filename = FilenameUtils.getName(fileDetails.getFileLocation().getFile());
-        File fileToDownload = new File(localFilePath(this.downloadedZipFileDirectory) + File.separator + filename);
+        final String filename = FilenameUtils.getName(fileDetails.getFileLocation().getFile());
         for (int n = 0; n < this.fileDownloadRetryAttempts; n++) {
-            try {
-                LOG.debug("Downloading '" + filename + "'...");
-                SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(this.fileDownloadReadTimeout).build();
-                RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(fileDownloadConnectTimeout).build();
-                HttpClientBuilder httpClientBuilder = HttpClients.custom()
-                        .setDefaultSocketConfig(socketConfig)
-                        .setDefaultRequestConfig(requestConfig)
-                        .disableContentCompression();
-                if (this.useSystemProxy) {
-                    DetectProxyConfig proxyConfig = new DetectProxyConfig();
-                    if (proxyConfig.isProxyAvailable()) {
-                        LOG.info("Setting http proxy to: " + proxyConfig.getHost() + ":" + proxyConfig.getPort());
-                        httpClientBuilder.setProxy(new HttpHost(proxyConfig.getHost(), proxyConfig.getPort()));
-                    }
-                }
-                CloseableHttpClient httpClient = httpClientBuilder.build();
-                CloseableHttpResponse fileDownloadResponse = httpClient.execute(new HttpGet(fileDetails.getFileLocation().toURI()));
-                try {
-                    HttpEntity remoteFileStream = fileDownloadResponse.getEntity();
-                    copyInputStreamToFile(remoteFileStream.getContent(), fileToDownload);
-                } finally {
-                    fileDownloadResponse.close();
-                }
 
+            File downloadedFile = fileDownloader.attemptToDownload(fileDetails.getFileLocation());
+
+            if (null != downloadedFile) {
                 if (!checkFileHash) {
-                    return fileToDownload;
+                    return downloadedFile;
                 }
 
-                LOG.info("Checking to see if downloaded copy of '" + fileToDownload.getName() + "' is valid.");
-                FileHashChecker fileHashChecker = new FileHashChecker(fileToDownload);
+                LOG.info("Checking to see if downloaded copy of '" + downloadedFile.getName() + "' is valid.");
+                FileHashChecker fileHashChecker = new FileHashChecker(downloadedFile);
                 fileHashChecker.setExpectedHash(fileDetails.getHash(), fileDetails.getHashType());
                 if (fileHashChecker.fileIsValid()) {
-                    return fileToDownload;
+                    return downloadedFile;
                 }
-            } catch (IOException ex) {
-                LOG.info("Problem downloading '" + fileToDownload.getName() + "'... " + ex.getCause().getLocalizedMessage());
-                if (n + 1 < this.fileDownloadRetryAttempts)
-                    LOG.info("Trying to download'" + fileToDownload.getName() + "' again...");
+            }
+
+            LOG.info("Problem downloading '" + filename + "'... ");
+
+            if (n + 1 < this.fileDownloadRetryAttempts) {
+                LOG.info("Trying to download'" + filename + "' again...");
             }
         }
 
-        LOG.error("Unable to successfully downloaded '" + fileToDownload.getName() + "'!");
-        throw new MojoExecutionException("Unable to successfully downloaded '" + fileToDownload.getName() + "'!");
-    }
-
-    /**
-     * Set the location directory where files will be downloaded to/
-     *
-     * @param downloadDirectory The directory that the file will be downloaded to.
-     */
-    private String localFilePath(File downloadDirectory) throws MojoFailureException {
-        if (downloadDirectory.exists()) {
-            if (downloadDirectory.isDirectory()) {
-                return downloadDirectory.getAbsolutePath();
-            } else {
-                throw new MojoFailureException("'" + downloadDirectory.getAbsolutePath() + "' is not a directory!");
-            }
-        }
-
-        if (downloadDirectory.mkdirs()) {
-            return downloadDirectory.getAbsolutePath();
-        } else {
-            throw new MojoFailureException("Unable to create download directory!");
-        }
+        throw new MojoExecutionException("Unable to successfully downloaded '" + filename + "'!");
     }
 }
---
+++ src/main/java/com/lazerycode/selenium/download/FileDownloader.java
@@ -0,0 +1,108 @@
+package com.lazerycode.selenium.download;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.log4j.Logger;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
+
+public class FileDownloader {
+
+    private static final Logger LOG = Logger.getLogger(FileDownloader.class);
+    private int fileDownloadReadTimeout;
+    private int fileDownloadConnectTimeout;
+    private boolean useSystemProxy = true;
+    String downloadDirectory;
+    DetectProxyConfig proxyConfig;
+
+
+    FileDownloader(File downloadedZipFileDirectory, boolean useSystemProxy) throws MojoFailureException {
+        this.useSystemProxy = useSystemProxy;
+        if (this.useSystemProxy) {
+            proxyConfig = new DetectProxyConfig();
+        }
+        this.downloadDirectory = localFilePath(downloadedZipFileDirectory);
+    }
+
+    public void setReadTimeout(int fileDownloadReadTimeout) {
+        this.fileDownloadReadTimeout = fileDownloadReadTimeout;
+    }
+
+    public void setConnectTimeout(int fileDownloadConnectTimeout) {
+        this.fileDownloadConnectTimeout = fileDownloadConnectTimeout;
+    }
+
+    /**
+     * Attempt to download a file
+     *
+     * @return File
+     * @throws java.lang.Exception
+     */
+    public File attemptToDownload(URL fileLocation) throws Exception {
+        String filename = FilenameUtils.getName(fileLocation.getFile());
+        SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(fileDownloadReadTimeout).build();
+
+        RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(fileDownloadConnectTimeout).build();
+
+        HttpClientBuilder httpClientBuilder = HttpClients.custom()
+                .setDefaultSocketConfig(socketConfig)
+                .setDefaultRequestConfig(requestConfig)
+                .disableContentCompression();
+
+        if (useSystemProxy && proxyConfig.isProxyAvailable()) {
+            LOG.info("Using http proxy: " + proxyConfig.getHost() + ":" + proxyConfig.getPort());
+            httpClientBuilder.setProxy(new HttpHost(proxyConfig.getHost(), proxyConfig.getPort()));
+        }
+
+        CloseableHttpClient httpClient = httpClientBuilder.build();
+
+        LOG.debug("Downloading '" + filename + "'...");
+        CloseableHttpResponse fileDownloadResponse = httpClient.execute(new HttpGet(fileLocation.toURI()));
+
+        HttpEntity remoteFileStream = fileDownloadResponse.getEntity();
+        File fileToDownload = new File(downloadDirectory + File.separator + filename);
+        try {
+            copyInputStreamToFile(remoteFileStream.getContent(), fileToDownload);
+        } catch (IOException ex) {
+            LOG.info("Problem downloading '" + filename + "'... " + ex.getCause().getLocalizedMessage());
+            fileToDownload = null;
+        } finally {
+            fileDownloadResponse.close();
+        }
+        return fileToDownload;
+    }
+
+    /**
+     * Set the location directory where files will be downloaded to
+     *
+     * @param downloadDirectory The directory that the file will be downloaded to.
+     */
+    private String localFilePath(File downloadDirectory) throws MojoFailureException {
+        if (downloadDirectory.exists()) {
+            if (downloadDirectory.isDirectory()) {
+                return downloadDirectory.getAbsolutePath();
+            } else {
+                throw new MojoFailureException("'" + downloadDirectory.getAbsolutePath() + "' is not a directory!");
+            }
+        }
+
+        if (downloadDirectory.mkdirs()) {
+            return downloadDirectory.getAbsolutePath();
+        } else {
+            throw new MojoFailureException("Unable to create download directory!");
+        }
+    }
+}
