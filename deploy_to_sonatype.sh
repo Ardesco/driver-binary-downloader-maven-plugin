bf832a819d0a6dc665e31d0a6456b24a6303cd98
Mark Collin <mark.collin@lazeryattack.com>
2016-02-20 11:14:03 +0000

    Move build and release process over to Travis-CI

---
+++ deploy_to_sonatype.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [[ "${TRAVIS_BRANCH}" == 'master' ]] ; then
+    mkdir -p ./target
+    MVNSETTINGS='
+    <settings>
+        <servers>
+            <server>
+                <id>sonatype-nexus-snapshots</id>
+                <username>'"${SONATYPE_USER}"'</username>
+                <password>'"${SONATYPE_PASS}"'</password>
+            </server>
+        </servers>
+    </settings>'
+    mvn deploy -s ./target/maven-settings.xml
+fi
